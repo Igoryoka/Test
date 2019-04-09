8deed8e9ca0c5999652c33fa872fff01eea3eb52
Home <igoryoka42@gmail.cpm>
2019-04-09 20:52:50 +0300

    Это наш первый комит

---
+++ script.js
@@ -0,0 +1,38 @@
+'use strict';
+
+a = 5;
+console.log(a);
+
+var leftBorderWidth = 12;
+let secod = 2;
+const pi = 3.14;
+
+let something;
+
+let persone = {
+    name: "Johnn",
+    age: 25,
+    isMarried: false
+};
+console.log(persone["name"]);
+
+let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+
+console.log(arr[0]);
+
+// alert('Hello World!');
+
+// let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+let answer = prompt("Are yoy 18 years old?", "Yea");
+
+console.log(typeof(null));
+
+console.log("2" === 2);
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
